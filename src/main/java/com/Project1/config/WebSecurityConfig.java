4cc6db0f863f635b64d5f232f890e250daba8c82
jlaws710 <jlaws710@gmail.com>
2022-10-14 22:47:50 -0700

    added security for register

--- src/main/java/com/Project1/config/WebSecurityConfig.java
+++ src/main/java/com/Project1/config/WebSecurityConfig.java
@@ -3,9 +3,11 @@ package com.Project1.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -54,9 +56,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.POST, "/register");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/register")
+                .antMatchers(HttpMethod.POST,"/register")
                 .permitAll()
                 .antMatchers("/user/{username}")
                 .hasAuthority("USER")
